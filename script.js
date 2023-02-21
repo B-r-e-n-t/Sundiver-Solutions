8c45c710c48ad8529b04db21e90006f6b5c0ed14
Brent <jbmsharpe@gmail.com>
2023-02-21 14:00:31 -0500

    navbar, hero, and section are all complete

---
+++ script.js
@@ -0,0 +1,28 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open')
+    })
+})
+
+
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => tabContent.classList.remove('active'))
+        target.classList.add('active');
+    })
+})
+
+
+
